6c9ad7b2d41fed90957471463cd293fb7660bd70
JAPole <yvr71@hotmail.co.uk>
2020-04-09 16:17:21 +0100

    maudy thurs code

--- application1/app.py
+++ application1/app.py
@@ -1,7 +1,4 @@
-
 from application import app
 
 if __name__== '__main__':
-    app.run(port=5000, host='0.0.0.0'
-
-#flask runs on 5000 anyway. we can change ports on other services
+    app.run(port=5000, host='0.0.0.0')
