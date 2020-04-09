3d9ea5cbb799b08a8e7369b9c3bfff8200fe774d
fabiafowler <fabiafowler@gmail.com>
2020-04-09 10:39:44 +0000

    made sure reponse is in correct format

--- application3/application/routes.py
+++ application3/application/routes.py
@@ -1,22 +1,19 @@
 from application import app
 from flask import request, Response
-# response is important to get a response
-#normally import render template etc but not needed here as we dont use html
 import random
 import math
 from random import randint
 
-#specify 2 numbers and it picks a random int between the 2
-@app.route('/generator/numbers', methods = ['GET'])
-def sixdig():
-    sixnum =  random.randint(100000,999999)
-    return str(sixnum)
-@app.route('/generator/8numbers', methods = ['GET'])
-def eightdig():
-    eightnum = random.randint(10000000,99999999)
-    return str(eightnum)
 
 
-print(sixdig())
-print(eightdig())
+@app.route('/generator/sixdig', methods = ['GET'])
+def get_sixdig():
+    return Response((str(random.randint(100000,999999))), mimetype='text/plain')
+
+@app.route('/generator/eightdig', methods = ['GET'])
+def get_eightdig():
+    return Response((str(random.randint(10000000,99999999))), mimetype='text/plain')
+
+print(get_sixdig())
+print(get_eightdig())
 
