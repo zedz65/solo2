26bf874cf9f47c110425bdf2f033a3ef7fa1bf3b
zedz65 <zedz65@hotmail.com>
2020-04-09 13:00:55 +0000

    service 2 complete

--- application2/application/routes.py
+++ application2/application/routes.py
@@ -1,6 +1,20 @@
 from application import app
 from flask import request, Response
-# response is important to get a response
-#normally import render template etc but not needed here as we dont use html
+import string
+import random
 from random import randint
-#specify 2 numbers and it picks a random int between the 2
+
+
+#route for three lowercase letters using get method 
+@app.route('/generator/three_lower', methods = ['GET']
+def three_lower():
+    result = ''.join(random.choices(string.ascii_lowercase,k=3))
+    return Response(result, mimetype='text/plain')
+
+
+#route for two uppercase letters for random generator using get
+@app.route('/generator/two_upper', methods = ['GET']
+def two_upper():
+    result = ''.join(random.choices(string.ascii_uppercase,k=2))
+    return Response(result, mimetype='text/plain')
+
