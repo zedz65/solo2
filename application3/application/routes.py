13eb9e879352947141d886748090c9fe04adf2cc
zedz65 <61311330+zedz65@users.noreply.github.com>
2020-04-20 21:15:42 +0100

    Update routes.py

--- application3/application/routes.py
+++ application3/application/routes.py
@@ -1,18 +1,19 @@
 from application import app
 from flask import request, Response
+import string
 import random
-import math
 from random import randint
 
 
+#route for three lowercase letters using get method 
+@app.route('/generator/three_lower', methods = ['GET'])
+def three_lower():
+    result = ''.join(random.choices(string.ascii_lowercase,k=3))
+    return Response(result, mimetype='text/plain')
 
-@app.route('/generator/sixdig', methods = ['GET'])
-def get_sixdig():
-    return Response((str(random.randint(100000,999999))), mimetype='text/plain')
 
-@app.route('/generator/eightdig', methods = ['GET'])
-def get_eightdig():
-    return Response((str(random.randint(10000000,99999999))), mimetype='text/plain')
-
-print(get_sixdig())
-print(get_eightdig())
+#route for two uppercase letters for random generator using get
+@app.route('/generator/two_upper', methods = ['GET'])
+def two_upper():
+    result = ''.join(random.choices(string.ascii_uppercase,k=2))
+    return Response(result, mimetype='text/plain')
