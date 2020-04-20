580e10ec71fce4523519c4b4c60990a49bce7f7f
zedz65 <61311330+zedz65@users.noreply.github.com>
2020-04-20 21:19:26 +0100

    Update routes.py

--- application2/application/routes.py
+++ application2/application/routes.py
@@ -6,15 +6,14 @@ from random import randint
 
 
 #route for three lowercase letters using get method 
-@app.route('/generator/three_lower', methods = ['GET']
+@app.route('/generator/three_lower', methods = ['GET'])
 def three_lower():
     result = ''.join(random.choices(string.ascii_lowercase,k=3))
     return Response(result, mimetype='text/plain')
 
 
 #route for two uppercase letters for random generator using get
-@app.route('/generator/two_upper', methods = ['GET']
+@app.route('/generator/two_upper', methods = ['GET'])
 def two_upper():
     result = ''.join(random.choices(string.ascii_uppercase,k=2))
     return Response(result, mimetype='text/plain')
-
