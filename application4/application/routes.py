34dbb96a14028b4f71016622c2e20191c780f36c
fabiafowler <fabiafowler@gmail.com>
2020-05-01 14:39:29 +0000

    service 4 running, need to fix bugs linking service 1 to 4

--- application4/application/routes.py
+++ application4/application/routes.py
@@ -4,38 +4,38 @@ from flask import request, Response
 #normally import render template etc but not needed here as we dont use html
 from random import randint
 #specify 2 numbers and it picks a random int between the 2
-
-@app.route('/prize', methods = ['POST'])
-def prizegen1():
-    if accountnumber[0] == 'a':
-        prize = "YOU ARE A WINNER!"    
-    elif accountnumber[0] == 'b':
+@app.route('/prizeshort', methods = ['POST'])
+def prizeshort():
+    if prizeusername[0] == 'a':
         prize = "YOU ARE A WINNER!"
-    elif accountnumber[0] == 'c':
+    elif prizeusername[0] == 'b':
         prize = "YOU ARE A WINNER!"
-    elif accountnumber[0] == 'd':
+    elif prizeusername[0] == 'c':
         prize = "YOU ARE A WINNER!"
-    elif accountnumber[0] == 'e':
+    elif prizeusername[0] == 'd':
         prize = "YOU ARE A WINNER!"
-    elif accountnumber[0] == 'f':
+    elif prizeusername[0] == 'e':
+        prize = "YOU ARE A WINNER!"
+    elif prizeusername[0] == 'f':
         prize = "YOU ARE A WINNER!"
     else:
         prize = "Try again next time"
     return Response(prize, mimetype= 'text/plain')
 
-def prizegen2():
-    if accountnumber[3] == '1':
+@app.route('/prizelong', methods = ['POST'])
+def prizelong():
+    if prizeusername[3] == '1':
         prize = "YOU ARE A WINNER!"
-    elif accountnumber[3] == '2':
+    elif prizeusername[3] == '2':
         prize = "YOU ARE A WINNER!"
-    elif accountnumber[3] == '3':
+    elif prizeusername[3] == '3':
         prize = "YOU ARE A WINNER!"
-    elif accountnumber[3] == '4':
+    elif prizeusername[3] == '4':
         prize = "YOU ARE A WINNER!"
-    elif accountnumber[3] == '5':
+    elif prizeusername[3] == '5':
         prize = "YOU ARE A WINNER!"
-    elif accountnumber[3] == '6':
+    elif prizeusername[3] == '6':
         prize = "YOU ARE A WINNER!"
     else:
         prize = "Try again next time"
-    return Response(noise, mimetype= 'text/plain')
+    return Response(prize, mimetype= 'text/plain')
