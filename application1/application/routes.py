5d369c3b0a8afe80fd6d2bfc166055cb3deac248
fabiafowler <fabiafowler@gmail.com>
2020-05-01 10:27:08 +0000

    display number on prize page

--- application1/application/routes.py
+++ application1/application/routes.py
@@ -4,18 +4,17 @@ import requests
 
 
 @app.route('/', methods=['GET'])   
-def accountref():
+def home():
 
     three_lower = requests.get('http://application2:5001/generator/three_lower')	 
     get_sixdig = requests.get('http://application3:5002/generator/sixdig')
  
     return render_template('home.html', title='home', three_lower=three_lower.text, get_sixdig=get_sixdig.text) 
 
-#post is to do with service 4 as it posts the generated letters & numbers - not sure yet how to do this but think it is:
-@app.route('/prize', methods=['GET', 'POST'])
+
+@app.route('/prize', methods=['GET'])
 def prize():
+    three_lower = requests.get('http://application2:5001/generator/three_lower')
+    get_sixdig = requests.get('http://application3:5002/generator/sixdig')
+    return render_template('prize.html', title='prize', three_lower=three_lower.text, get_sixdig=get_sixdig.text)
 
-    three_lower = requests.post('http://application2:5003/generator/three_lower', data= three_lower.text)
-    get_sixdig = requests.post('http://application3:5003/generator/get_sixdig', data=get_sixdig.text)
-
-    return render_template('prize.html', title='Prize', three_lower=three_lower.text, get_sixdig=get_sixdig.text)
