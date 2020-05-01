3ce30fbb328f3eeace58cbbc522004aac01c1c82
fabiafowler <fabiafowler@gmail.com>
2020-05-01 11:38:15 +0000

    prize generator function on app4)

--- application4/application/routes.py
+++ application4/application/routes.py
@@ -5,4 +5,37 @@ from flask import request, Response
 from random import randint
 #specify 2 numbers and it picks a random int between the 2
 
+@app.route('/prize', methods = ['POST'])
+def prizegen1():
+    if accountnumber[0] == 'a':
+        prize = "YOU ARE A WINNER!"    
+    elif accountnumber[0] == 'b':
+        prize = "YOU ARE A WINNER!"
+    elif accountnumber[0] == 'c':
+        prize = "YOU ARE A WINNER!"
+    elif accountnumber[0] == 'd':
+        prize = "YOU ARE A WINNER!"
+    elif accountnumber[0] == 'e':
+        prize = "YOU ARE A WINNER!"
+    elif accountnumber[0] == 'f':
+        prize = "YOU ARE A WINNER!"
+    else:
+        prize = "Try again next time"
+    return Response(noise, mimetype= 'text/plain')
 
+def prizegen2():
+    if accountnumber[3] == '1':
+        prize = "YOU ARE A WINNER!"
+    elif accountnumber[3] == '2':
+        prize = "YOU ARE A WINNER!"
+    elif accountnumber[3] == '3':
+        prize = "YOU ARE A WINNER!"
+    elif accountnumber[3] == '4':
+        prize = "YOU ARE A WINNER!"
+    elif accountnumber[3] == '5':
+        prize = "YOU ARE A WINNER!"
+    elif accountnumber[3] == '6':
+        prize = "YOU ARE A WINNER!"
+    else:
+        prize = "Try again next time"
+    return Response(noise, mimetype= 'text/plain')
