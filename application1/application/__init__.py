4dd7c76846f845c1aa5442f7aa346f0f4709ed7b
zedz65 <61311330+zedz65@users.noreply.github.com>
2020-05-04 23:41:12 +0100

    Update __init__.py

--- application1/application/__init__.py
+++ application1/application/__init__.py
@@ -1,5 +1,19 @@
 from flask import Flask
+from os import getenv
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
 #object gets created from the class of FLask import name
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+
+db = SQLAlchemy(app)
+
+
+
+
 from application import routes
 # this needs to always be at the end or it will be an infinite loop
