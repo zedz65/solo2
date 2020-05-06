de9f44a8abb879e6b7ab7c367b01b2d776440e7c
zedz65 <zedz65@hotmail.com>
2020-05-06 11:26:46 +0100

    database working

--- application1/application/models.py
+++ application1/application/models.py
@@ -2,5 +2,5 @@ from application import db
 
 class Prize(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), nullable=False)
-    prize = db.Column(db.String(5), nullable=False)
+    username = db.Column(db.String(30), nullable=False)
+    prize = db.Column(db.String(500), nullable=False)
--- application1/application/routes.py
+++ application1/application/routes.py
@@ -16,7 +16,7 @@ def home():
 @app.route('/prize/<prizeusername>', methods=['GET', 'POST'])
 def prize(prizeusername):
     prize = requests.post('http://application4:5003/prizeshort', data=prizeusername)
-    user_data = Prize(username=username, prize=prize)
+    user_data = Prize(username=prizeusername, prize=prize.text)
     db.session.add(user_data)
     db.session.commit()
 
